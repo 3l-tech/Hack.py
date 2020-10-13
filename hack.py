9286773fa49f5940952ae0eb6c9168e8416cb549
3l-tech <68188212+3l-tech@users.noreply.github.com>
2020-10-13 16:10:39 +0100

    Create hack.py

---
+++ hack.py
@@ -0,0 +1,80 @@
+import time #lets you use time.sle
+print("--------------------\nDownloading Free Ram\n--------------------") #Introduction text
+print("███████████████████████████████\n████╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬████\n██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██\n█╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█\n█╬╬╬███████╬╬╬╬╬╬╬╬╬███████╬╬╬█\n█╬╬██╬╬╬╬███╬╬╬╬╬╬╬███╬╬╬╬██╬╬█\n█╬██╬╬╬╬╬╬╬██╬╬╬╬╬██╬╬╬╬╬╬╬██╬█\n█╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█\n█╬╬╬╬█████╬╬╬╬╬╬╬╬╬╬╬█████╬╬╬╬█\n█╬╬█████████╬╬╬╬╬╬╬█████████╬╬█\n█╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█\n█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█\n█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█\n█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█\n█╬╬╬▓▓▓▓╬╬╬╬╬╬╬█╬╬╬╬╬╬╬▓▓▓▓╬╬╬█\n█╬╬▓▓▓▓▓▓╬╬█╬╬╬█╬╬╬█╬╬▓▓▓▓▓▓╬╬█\n█╬╬╬▓▓▓▓╬╬██╬╬╬█╬╬╬██╬╬▓▓▓▓╬╬╬█\n█╬╬╬╬╬╬╬╬██╬╬╬╬█╬╬╬╬██╬╬╬╬╬╬╬╬█\n█╬╬╬╬╬████╬╬╬╬███╬╬╬╬████╬╬╬╬╬█\n█╬╬╬╬╬╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬╬╬╬╬╬█\n██╬╬█╬╬╬╬╬╬╬╬█████╬╬╬╬╬╬╬╬█╬╬██\n██╬╬██╬╬╬╬╬╬███████╬╬╬╬╬╬██╬╬██\n██╬╬▓███╬╬╬████╬████╬╬╬███▓╬╬██\n███╬╬▓▓███████╬╬╬███████▓▓╬╬███\n███╬╬╬╬▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬███\n████╬╬╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬╬╬████\n█████╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬█████\n██████╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬██████\n███████╬╬╬╬╬╬╬███╬╬╬╬╬╬╬███████\n████████╬╬╬╬╬╬███╬╬╬╬╬╬████████\n█████████╬╬╬╬╬███╬╬╬╬╬█████████\n███████████╬╬╬╬█╬╬╬╬███████████\n███████████████████████████████")
+#^ anonymous logo
+time.sleep(2) #time function
+print("Hacking into your computer...") #first section
+time.sleep(2) #time function
+print("=======================\nHacking proccess... 10%") #"interactive" section
+time.sleep(1.5) #time function
+print("Hacking proccess... 15%")#IS
+time.sleep(1) #time function
+print("Hacking proccess... 28%")#IS
+time.sleep(.5) #time function
+print("Hacking proccess... 46%")#IS
+time.sleep(2.5) #time function
+print("Hacking proccess... 61%")#IS
+time.sleep(2) #time function
+print("Hacking proccess... 83%")#IS
+time.sleep(1) #time function
+print("Hacking proccess... 100%\n========================")#IS
+time.sleep(3) #time function
+print("+++++++++++++++++++++++++++++++++\nInstalling malware into computer.") #second section
+time.sleep(1.5) #time function
+print("Stealing saved information on websites.") #second section
+time.sleep(1.5) #time function
+print("Activating front camera.") #second section
+print()#spacer
+print()#spacer
+print("________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__________\n______¶¶¶¶¶¶_____________¶¶¶¶¶¶________ \n_____¶¶¶¶¶_________________¶¶¶¶¶¶______ \n____¶¶¶¶_____________________¶¶¶¶¶_____ \n___¶¶¶¶_______________________¶¶¶¶¶____ \n__¶¶¶¶_____¶¶¶¶_______¶¶¶¶______¶¶¶____ \n__¶¶¶_____¶¶¶¶¶¶_____¶¶¶¶¶¶_____¶¶¶¶___ \n_¶¶¶¶_____¶¶¶¶¶¶_____¶¶¶¶¶¶______¶¶¶___ \n_¶¶¶_______¶¶¶¶_______¶¶¶¶_______¶¶¶¶__ \n_¶¶¶______________________________¶¶¶__ \n_¶¶¶______________________________¶¶¶__ \n_¶¶¶______________________________¶¶¶__ \n_¶¶¶____________¶¶¶¶¶____________¶¶¶¶__ \n_¶¶¶¶________¶¶¶¶¶¶¶¶¶¶¶_________¶¶¶___ \n__¶¶¶______¶¶¶¶¶_____¶¶¶¶¶______¶¶¶¶___ \n__¶¶¶¶____¶¶¶___________¶¶¶____¶¶¶¶____ \n___¶¶¶¶___¶¶_____________¶¶___¶¶¶¶_____ \n____¶¶¶¶____________________¶¶¶¶¶______ \n_____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_______ \n_______¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_________")
+#^ "users face"
+print()#spacer
+print()#spacer
+time.sleep(.9) #time function
+print("Downloading and sending pictures to host.") #second section
+time.sleep(2) #time function
+print("Disconnecting you from WIFI connection and wiping TCP history.\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")#second section
+time.sleep(3) #time function
+print("Stealing personal identity and information from drive.\nAquiring IP adress and payment methods related to it.\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++") #third section
+print("Injecting Destruction.JS") #final section
+time.sleep(1.5) #time function
+print("PIN:4325") #final section
+time.sleep(.5) #time function
+print("PIN:2315") #final section
+time.sleep(.5) #time function
+print("PIN:4266") #final section
+time.sleep(.5) #time function
+print("PIN:682") #final section
+time.sleep(.5) #time function
+print("PIN:13596") #final section
+time.sleep(.5) #time function
+print("PIN:41601") #final section
+time.sleep(.5) #time function
+print("PIN:40197601") #final section
+time.sleep(.5) #time function
+print("PIN:31561461") #final section
+time.sleep(.5) #time function
+print("PIN:624361") #final section
+time.sleep(.5) #time function
+print("PIN:31535") #final section
+time.sleep(.5) #time function
+print("PIN:21341") #final section
+time.sleep(.5) #time function
+print("PIN:416903") #final section
+time.sleep(.5) #time function
+print("PIN:001") #final section
+time.sleep(.5) #time function
+print("PIN:0423161") #final section
+time.sleep(.5) #time }")
+Hacking = True#activates while loop
+while Hacking == True:#while loop 
+  print("010001000001110001101010101010101111000010010101010100101101010101001010101011001000101010110000")#"the hacker language"(binary lol)
+  time.sleep(.25)#time function
+  print("100010010100101110001010100110011010100011100100000101010100101010101010001001010110100101000000")#"the hacker language"(binary lol)
+  time.sleep(.25)#time function    
+  print("010100101101010101001010101011001000101010110000000101010100101010101010001001010110100101000000")#"the hacker language"(binary lol)
+  time.sleep(.25)#time function
+  print("001000100010000100101101011000001101001110011011100001001001001010101010101010000011100000000110")#"the hacker language"(binary lol)
+  time.sleep(.25)#time function
+  print("000101010100101010101010001001010110100101000000001000100010000100101101011000001101001110011011")#"the hacker language"(binary lol)
+  time.sleep(.35)#time function
